b47ae1daed9a948ebd7fab10e3149a2f4fa604d7
Shamil <shamilgadzhimirzaev@gmail.com>
2025-06-21 22:49:16 +0300

    Изменение репозитория в соответствии с абстрактным классом

---
+++ SolutionAkvatoria/FastAbstractObject.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NavalTacticalModel
+{
+    public abstract class FastAbstractObject
+    {
+        public string uid = Guid.NewGuid().ToString();
+        [XmlElement(Type = typeof(TimeSpan))]
+        public TimeSpan lastUpdated;
+        public abstract (TimeSpan, FastAbstractEvent) getNearestEvent();
+        public abstract void Update(TimeSpan timeSpan);
+    }
+
+    public abstract class FastAbstractEvent
+    {
+        public string objId = null;
+        public abstract void runEvent(FastAbstractWrapper wrapper, TimeSpan timeSpan);
+    }
+
+    public abstract class FastAbstractWrapper
+    {
+        protected Dictionary<string, FastAbstractObject> objects = new Dictionary<string, FastAbstractObject>();
+
+        protected SortedList<TimeSpan, FastAbstractEvent> eventList = new SortedList<TimeSpan, FastAbstractEvent>();
+        protected Dictionary<string, TimeSpan> objectsEventTime = new Dictionary<string, TimeSpan>();
+        public TimeSpan updatedTime;
+        private HashSet<string> objectsKeyForUpdate;
+
+        protected Action<string> writeDebug = Console.WriteLine;
+        public Action<string> writeError = Console.WriteLine;
+
+        public bool isDebug = false;
+
+        public void WriteDebug(string debug)
+        {
+            if (isDebug)
+                writeDebug(debug);
+        }
+
+        public TimeSpan GetNearEventTime()
+        {
+            if (eventList.Count == 0)
+                return TimeSpan.MaxValue;
+            return eventList.First().Key;
+        }
+
+        public void RemoveObjects(string uid)
+        {
+            objects.Remove(uid);
+            objectsEventTime.Remove(uid);
+            objectsKeyForUpdate.Remove(uid);
+        }
+
+        public List<string> getObjectKeys()
+        {
+            return objects.Keys.ToList();
+        }
+
+        public FastAbstractObject getObject(string key)
+        {
+            var obj = objects[key];
+            obj.Update(updatedTime);
+            objectsKeyForUpdate.Add(key);
+            return objects[key];
+        }
+
+        protected TimeSpan AddEvent(TimeSpan timeSpan, FastAbstractEvent modelEvent, string objUid = null)
+        {
+            WriteDebug($"Event added {timeSpan} {modelEvent} {objUid}");
+
+            if (timeSpan < updatedTime)
+                throw new Exception("Time error!!");
+            while (eventList.ContainsKey(timeSpan))
+            {
+                timeSpan = timeSpan.Add(TimeSpan.FromTicks(1));
+            }
+            modelEvent.objId = objUid;
+            eventList.Add(timeSpan, modelEvent);
+            if (!(objUid is null))
+            {
+                objectsEventTime.Add(objUid, timeSpan);
+            }
+            return timeSpan;
+        }
+
+        public bool Next()
+        {
+            if (eventList.Count == 0)
+                return false;
+            objectsKeyForUpdate = new HashSet<string>();
+            var task = eventList.First();
+            updatedTime = task.Key;
+            eventList.Remove(task.Key);
+            if (!(task.Value.objId is null))
+            {
+                getObject(task.Value.objId);
+            }
+            WriteDebug($"Run Event {task}");
+            task.Value.runEvent(this, task.Key);
+            foreach (var objKey in objectsKeyForUpdate)
+            {
+                if (objectsEventTime.ContainsKey(objKey))
+                {
+                    eventList.Remove(objectsEventTime[objKey]);
+                    objectsEventTime.Remove(objKey);
+                }
+                var ev = objects[objKey].getNearestEvent();
+                if (!(ev.Item2 is null))
+                {
+                    AddEvent(ev.Item1, ev.Item2, objKey);
+                }
+            }
+            return true;
+        }
+
+        public void addObject(FastAbstractObject obj)
+        {
+            objects.Add(obj.uid, obj);
+            var ev = obj.getNearestEvent();
+            if (!(ev.Item2 is null))
+            {
+                AddEvent(ev.Item1, ev.Item2, obj.uid);
+            }
+        }
+
+        public List<FastAbstractObject> GetFilteredObjects(Func<FastAbstractObject, bool> filterCriteria)
+        {
+            return objects.Values.Where(filterCriteria).ToList();
+        }
+
+        public List<FastAbstractObject> GetObjectsForUpdate()
+        {
+            if (objectsKeyForUpdate == null || objectsKeyForUpdate.Count == 0)
+                return new List<FastAbstractObject>();
+
+            return objects.Values
+                         .Where(obj => obj != null && objectsKeyForUpdate.Contains(obj.uid))
+                         .ToList();
+        }
+
+    }
+
+
+}
+
--- SolutionAkvatoria/Program.cs
+++ SolutionAkvatoria/Program.cs
@@ -1,59 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NavalTacticalModel
 {
-    class Program
+     class Program
     {
-        static void Main(string[] args)
-        {
-            // Параметры из таблицы 3.1
-            double R0 = 1000; // радиус соты
-            double R1 = 1000; // дальность обнаружения на бензине
-            double R2 = 500;  // дальность обнаружения на электричестве
-            double aTrA = 5;   // ускорение БЭК типа A
-            double V1A = 10;   // скорость на бензине
-            double V2A = 3;    // скорость на электричестве
-            double RFPV = 5000; // радиус поражения
 
-            // Создаем симуляцию
-            var simulation = new TacticalSimulation(
-                zoneG: new Vector2D(10000, 500), // зона G
-                simulationTimeStep: 60); // шаг времени 60 секунд
 
-            // Добавляем буи (случайным образом с вероятностью 0.75)
-            Random rand = new Random();
-            int buoyCount = 0;
-
-            for (int i = -5; i <= 5; i++)
-            {
-                for (int j = -5; j <= 5; j++)
-                {
-                    if (rand.NextDouble() < 0.75) // вероятность установки буя
-                    {
-                        double x = i * R0 * 1.5;
-                        double y = j * R0 * Math.Sqrt(3);
-                        simulation.AddBuoy(new Buoy(
-                            $"B{++buoyCount:000}",
-                            new Vector2D(x, y),
-                            R1, R2));
-                    }
-                }
-            }
-
-            // Добавляем БЭК типа A
-            simulation.AddUSV(new USV(
-                "A001",
-                startPosition: new Vector2D(-3 * R0, 0), // начальная позиция в зоне B
-                maxAcceleration: aTrA,
-                gasolineSpeed: V1A,
-                electricSpeed: V2A));
-
-            // Запускаем симуляцию на 1 час
-            simulation.RunSimulation(totalSimulationTime: 3600);
-
-            // Выводим результаты
-            simulation.PrintSimulationResults();
-            simulation.PrintEventLog();
-        }
     }
 }
